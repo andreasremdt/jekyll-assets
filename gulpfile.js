d4787f75eade7c0d322e5c9bf44d513a6f028b0c
Andreas Remdt <andreas.remdt@gmail.com>
2018-04-15 13:31:41 -0400

    Added JS processing and implemented Jekyll

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@
  * and SASS files into production-ready code.
  * 
  * Copyright (c) 2018 Andreas Remdt
- * Licensed under ISC
+ * MIT License
  */
 
 'use strict';
@@ -16,8 +16,16 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const webp = require('gulp-webp');
 const image = require('gulp-image');
+const util = require('gulp-util');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const gulpif = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
 const path = require('path');
 const del = require('del');
+const child = require('child_process');
+const browserSync = require('browser-sync').create();
 
 // Load local config
 const config = require('./config.json');
@@ -33,6 +41,27 @@ gulp.task('sass', () => {
     .src(path.join(__dirname, config.sass.inputDir, '**', '*.{scss,sass}'))
     .pipe(sass(config.sass.options).on('error', sass.logError))
     .pipe(gulp.dest(path.join(__dirname, config.sass.outputDir)));
+});
+
+
+
+/**
+ * Processes the JavaScript. If specified in the config, can:
+ * 
+ * a) Generate sourcemaps
+ * b) Compile ES2015 or higher into ES5
+ * c) Concat many JS files into a single file
+ * d) Minify the output
+ */
+gulp.task('js', () => {
+  return gulp
+    .src(path.join(__dirname, config.js.src, '**', '*.js'))
+    .pipe(gulpif(config.js.sourcemaps, sourcemaps.init()))
+    .pipe(gulpif(config.js.babel, babel()))
+    .pipe(gulpif(config.js.concat, concat(config.js.output.name)))
+    .pipe(gulpif(config.js.minify, uglify()))
+    .pipe(gulpif(config.js.sourcemaps, sourcemaps.write('.')))
+    .pipe(gulp.dest(path.join(__dirname, config.js.output.dir)));
 });
 
 
@@ -70,10 +99,46 @@ gulp.task('images', () => {
 gulp.task('clean', () => {
   return del([
     path.join(__dirname, config.sass.outputDir),
-    path.join(__dirname, config.images.outputDir)
+    path.join(__dirname, config.images.outputDir),
+    path.join(__dirname, config.js.output.dir),
+    path.join(__dirname, config.jekyll.baseDir)
   ]);
 });
 
 
 
+/**
+ * Runs the Jekyll build command with given commands and logs
+ * the output (whether error or info) to the console.
+ */
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['build'].concat(config.jekyll.args));
+
+  const logger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => util.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', logger);
+  jekyll.stderr.on('data', logger);
+});
+
+
+
+/**
+ * Runs the BrowserSync server to serve the site directory
+ * (usually _site). Reloads automatically.
+ */
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [`${config.jekyll.baseDir}/**`],
+    port: config.jekyll.port,
+    server: {
+      baseDir: config.jekyll.baseDir
+    }
+  });
+});
+
+gulp.task('develop', ['jekyll', 'serve']);
 gulp.task('default', ['sass', 'webp', 'images']);
